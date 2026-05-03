e26e305009c352a14c8737d3d5e395155096c1e2
Alvin <1545916566@qq.com>
2026-05-03 18:40:43 +0100

    fix: correct chef dishes link in detail dialog
    - Change '查看全部菜品' link from /menu to /chefs/[id]
    - Shows chef's dishes instead of all dishes

--- components/chef-detail-dialog.tsx
+++ components/chef-detail-dialog.tsx
@@ -137,7 +137,7 @@ export function ChefDetailDialog({ chef, open, onOpenChange }: ChefDetailDialogP
           )}
 
           <div className="pt-2">
-            <Link href={`/menu`} onClick={() => onOpenChange(false)}>
+            <Link href={`/chefs/${chef.id}`} onClick={() => onOpenChange(false)}>
               <Button variant="outline" className="w-full gap-2">
                 <UtensilsCrossed className="h-4 w-4" />
                 查看全部菜品
